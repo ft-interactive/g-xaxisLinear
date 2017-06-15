f008e47f2cd79bcc4131511b15317c6c4b19fd5c
Bob Haslett <bob.haslett@ft.com>
2017-06-15 16:35:03 +0100

    Update build

--- src/xaxisLinear.js
+++ src/xaxisLinear.js
@@ -1,17 +1,69 @@
+import * as d3 from 'd3';
+
 export default function() {
-  
+	let scale = d3.scaleLinear()
+        .domain([0,100])
+        .range([0,220]);
+    let tickSize = 50;
+    let offset = 0;
+    let numTicks = 5
 
+    function axis(parent) {
 
-  let labelWidth = 0;
-	  function axis(parent) {
+        const xAxis =d3.axisBottom()
+            .tickSize(tickSize*.75)
+            .ticks(numTicks)
+            .scale(scale)
+        
+        if (offset==0) {
+            xAxis.tickSize(tickSize)
+        }
 
-	  }
-  
-	  axis.labelWidth = (d)=>{
-	        if(d===undefined) return labelWidth
-	        labelWidth=d;
-	        return axis;
-	  }
+        const xLabel = parent.append("g")
+            .attr("class", "axis xAxis" )
+            .call(xAxis)
+        
+        if (offset>0) {
+            xLabel.attr("transform","translate(0,"+(offset)+")");
+            let ticks = xLabel.selectAll(".tick");
+            ticks.each(function (d) {
+                d3.select(this)
+                .classed("baseline",true);
+            })
+        }
+        
+    }
 
-  return axis
+    axis.scale = (d)=>{
+        scale = d;
+        return axis;
+        }
+        axis.domain = (d)=>{
+            scale.domain(d);
+            return axis;
+        };
+        axis.range = (d)=>{
+            scale.range(d);
+            return axis;
+        };
+        
+        axis.tickSize = (d)=>{
+            if(d===undefined) return tickSize
+            tickSize=d;
+            return axis;
+        }
+
+        axis.offset = (d)=>{
+            if(d===undefined) return offset
+            offset=d;
+            return axis;
+        }
+
+        axis.numTicks = (d)=>{
+            if(d===undefined) return numTicks
+            numTicks=d;
+            return axis;
+        }
+
+    return axis
 };
--- test/xaxisLinear-test.js
+++ test/xaxisLinear-test.js
@@ -2,6 +2,6 @@ var tape = require("tape"),
     axis = require("../");
 
 tape("foo() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
-   test.equal(axis.xaxisLinear().labelWidth(), 0);
+  test.equal(axis.xaxisLinear().tickSize(), 50);
   test.end();
 });
